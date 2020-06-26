ee444fd83771fb9c99540204be2e4efd79eff89a
elvolt <sidelevcs@gmail.com>
2020-06-26 20:15:34 +0300

    9.2. Checkstyle [#312524]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -4,9 +4,11 @@ public class Converter {
     public static int rubleToEuro(int value) {
         return value / 70;
     }
+
     public static int rubleToDollar(int value) {
         return value / 60;
     }
+
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
         int dollars = Converter.rubleToDollar(180);
