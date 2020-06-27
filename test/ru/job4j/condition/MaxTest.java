01b5936dfbbbfaf2e20cb26e7fe46482ae43d496
elvolt <sidelevcs@gmail.com>
2020-06-27 10:35:27 +0300

    4.2. Тернарное сравнение [#312474]

---
+++ test/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+
+    @Test
+    public void whenLeftMoreThanRightThenLeft() {
+        int left = 3;
+        int right = 2;
+        int result = Max.max(left, right);
+        assertThat(result, is(left));
+    }
+
+    @Test
+    public void whenRightMoreThanLeftThenRight() {
+        int left = -1;
+        int right = 2;
+        int result = Max.max(left, right);
+        assertThat(result, is(right));
+    }
+
+    @Test
+    public void whenLeftEqualsRightThenLeft() {
+        int left = 3;
+        int right = 3;
+        int result = Max.max(left, right);
+        assertThat(result, is(left));
+    }
+}
