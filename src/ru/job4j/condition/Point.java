ee21b53d514deac98dc77e429814d7a84d92ffab
elvolt <sidelevcs@gmail.com>
2020-07-03 21:46:17 +0300

    2.1. Рефакторинг - Расстояние между точками. [#312643]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,20 +1,27 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
 
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    private int x;
+
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
-        double result1 = Point.distance(1, 2, 3, 4);
-        double result2 = Point.distance(-1, 2, 2, -1);
-        double result3 = Point.distance(2, 3, 1, 4);
-        double result4 = Point.distance(6, 4, -3, -2);
-        System.out.println("result (1, 2) to (3, 4) " + result1);
-        System.out.println("result (-1, 2) to (2, -1) " + result2);
-        System.out.println("result (2, 3) to (1, 4) " + result3);
-        System.out.println("result (6, 4) to (-3, -2) " + result4);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
-
 }
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -9,23 +9,19 @@ public class PointTest {
 
     @Test
     public void whenPoint1EqualsPoint2ThenZero() {
-        int x1 = 3;
-        int y1 = 4;
-        int x2 = 3;
-        int y2 = 4;
+        Point point1 = new Point(3, 4);
+        Point point2 = new Point(3, 4);
         double expected = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = point1.distance(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void whenPoint1NotEqualsPoint2ThenMoreThanZero() {
-        int x1 = 1;
-        int y1 = 4;
-        int x2 = 2;
-        int y2 = -1;
+        Point point1 = new Point(1, 4);
+        Point point2 = new Point(2, -1);
         double expected = 5.1;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = point1.distance(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
