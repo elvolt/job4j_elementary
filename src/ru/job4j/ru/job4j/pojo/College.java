21f801c9c9cdd2fa58a059b91ef8edf31d543a0c
elvolt <sidelevcs@gmail.com>
2020-07-05 15:43:27 +0300

    2. Модель данных. [#312679]

---
+++ src/ru/job4j/ru/job4j/pojo/College.java
@@ -0,0 +1,19 @@
+package ru.job4j.ru.job4j.pojo;
+
+import java.time.LocalDate;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Ivan");
+        student.setSurname("Ivanov");
+        student.setPatronymic("Ivanovich");
+        student.setGroupNum(2);
+        student.setDateOfAdmission(LocalDate.of(1992, 12, 2));
+
+        System.out.println("Student: "
+                + String.join(" ", student.getSurname(), student.getName(), student.getPatronymic())
+                + ".\nDate of admission: " + student.getDateOfAdmission()
+                + ".\nGroup: " + student.getGroupNum());
+    }
+}
---
+++ src/ru/job4j/ru/job4j/pojo/Student.java
@@ -0,0 +1,51 @@
+package ru.job4j.ru.job4j.pojo;
+
+import java.time.LocalDate;
+
+public class Student {
+    private String name;
+    private String surname;
+    private String patronymic;
+    private int groupNum;
+    private LocalDate dateOfAdmission;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public int getGroupNum() {
+        return groupNum;
+    }
+
+    public void setGroupNum(int groupNum) {
+        this.groupNum = groupNum;
+    }
+
+    public LocalDate getDateOfAdmission() {
+        return dateOfAdmission;
+    }
+
+    public void setDateOfAdmission(LocalDate dateOfAdmission) {
+        this.dateOfAdmission = dateOfAdmission;
+    }
+}
