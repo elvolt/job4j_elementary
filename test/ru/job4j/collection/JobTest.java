0ea4db677d119f0e628eed4005e6d78c54b7c794
elvolt <sidelevcs@gmail.com>
2020-07-22 14:29:05 +0300

    2. Комбинированный компаратор [#312588]

--- test/ru/job4j/collection/JobTest.java
+++ test/ru/job4j/collection/JobTest.java
@@ -1,10 +1,13 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
-import java.util.Comparator;
 
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.lessThan;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class JobTest {
@@ -49,12 +52,36 @@ public class JobTest {
     }
 
     @Test
-    public void whenCompatorByNameAndPriority() {
+    public void whenCompatorByNameAndPriorityDesc() {
         Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
-        int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
-        );
-        assertThat(rsl, lessThan(0));
+        List<Job> jobs = new ArrayList<>();
+        Job job1 = new Job("Impl task", 0);
+        Job job2 = new Job("Fix bug", 1);
+        Job job3 = new Job("Fix bug", 2);
+        jobs.add(job1);
+        jobs.add(job2);
+        jobs.add(job3);
+        jobs.sort(cmpNamePriority);
+        Iterator<Job> it = jobs.iterator();
+        assertThat(it.next(), is(job1));
+        assertThat(it.next(), is(job3));
+        assertThat(it.next(), is(job2));
+    }
+
+    @Test
+    public void whenCompatorByNameAndPriorityAsc() {
+        Comparator<Job> cmpNamePriority = new JobAscByName().thenComparing(new JobAscByPriority());
+        List<Job> jobs = new ArrayList<>();
+        Job job1 = new Job("Impl task", 0);
+        Job job2 = new Job("Fix bug", 1);
+        Job job3 = new Job("Fix bug", 2);
+        jobs.add(job1);
+        jobs.add(job2);
+        jobs.add(job3);
+        jobs.sort(cmpNamePriority);
+        Iterator<Job> it = jobs.iterator();
+        assertThat(it.next(), is(job2));
+        assertThat(it.next(), is(job3));
+        assertThat(it.next(), is(job1));
     }
 }
