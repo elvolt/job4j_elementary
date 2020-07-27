9a2b31d9cbf6eec2409c24425993606e05371f71
elvolt <sidelevcs@gmail.com>
2020-07-27 20:15:04 +0300

    Отсортировать департаменты [#312582]

--- src/ru/job4j/collection/DepDescComp.java
+++ src/ru/job4j/collection/DepDescComp.java
@@ -8,20 +8,8 @@ public class DepDescComp implements Comparator<String> {
         String[] o1Deps = o1.split("/");
         String[] o2Deps = o2.split("/");
         int rsl = o2Deps[0].compareTo(o1Deps[0]);
-        if (rsl != 0) {
-            return rsl;
-        }
-        int length1 = o1Deps.length;
-        int length2 = o2Deps.length;
-        for (int i = 1; i < Math.min(length1, length2); i++) {
-            if (o1Deps[i].equals(o2Deps[i])) {
-                continue;
-            }
-            rsl = o1Deps[i].compareTo(o2Deps[i]);
-            break;
-        }
-        if (rsl == 0 && length1 != length2) {
-            rsl = length1 - length2;
+        if (rsl == 0) {
+            rsl = o1.compareTo(o2);
         }
         return rsl;
     }
